1e6d4f31c04b2653df3377d539422601f255b3e7
AbimaelFrontado <167887266+AbimaelFrontado@users.noreply.github.com>
2025-10-19 17:31:28 -0500

    Create MainActivity.kt

---
+++ Parcial/com.abi.parcial/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.abi.parcial
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.abi.parcial.R
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+    }
+}
